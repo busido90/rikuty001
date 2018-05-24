c8cda37337478787bd84bf16f8537ea0ead4fe5b
永山 大志 <tnagayama@geechs.com>
2018-05-24 15:58:16 +0900

    タイトル画面など追加

---
+++ Assets/Plugins/Gamestrap/UI/Examples/Complete Project/Global/Scripts/ESceneNames.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Gamestrap
+{
+    /// <summary>
+    /// List of scenes used in the Example
+    /// Make sure you add the scenes in the Build or else the transitions won't work
+    /// </summary>
+    public enum ESceneNames
+    {
+        MainMenu,
+        Levels,
+        Gameplay,
+		scene_track_1
+    }
+}
