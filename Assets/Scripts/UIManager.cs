61a2748aaf34060cd6685fab7c3001b55e2b4f8b
永山 大志 <nagatai0120@gmail.com>
2018-06-14 11:44:13 +0900

    諸々

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,12 @@ public class UIManager : MonoBehaviour {
 
 	[SerializeField] private Text txtZ;
 
+	[SerializeField] private Text txtIsConnect;
+
+	[SerializeField] private Text txtControllerAxisX;
+	[SerializeField] private Text txtControllerAxisY;
+
+
 	[SerializeField] private LogSave log;
 
 	private string txtLog;
@@ -27,9 +33,18 @@ public class UIManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		OVRInput.Update();
+
+
 		this.txtX.text = Input.acceleration.x.ToString();	
 		this.txtY.text = Input.acceleration.y.ToString();	
-		this.txtZ.text = Input.acceleration.z.ToString();	
+		this.txtZ.text = Input.acceleration.z.ToString();
+		this.txtIsConnect.text = OVRInput.IsControllerConnected(OVRInput.Controller.All).ToString();
+		Vector2 axis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.All);
+		this.txtControllerAxisX.text = axis.x.ToString();
+		this.txtControllerAxisY.text = axis.y.ToString();
+
+
 		txtLog += this.txtX.text + "," + this.txtY.text + "," + this.txtZ.text + "\n";
 		this.updateCount++;
 		if(this.updateCount == 300){
@@ -37,4 +52,7 @@ public class UIManager : MonoBehaviour {
 		}
 
 	}
+
+
+
 }
